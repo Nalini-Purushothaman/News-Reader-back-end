8592447f09db5c0afa7bbeb61f81669c8e28525c
nalini-purushothaman <pnalini90sri@gmail.com>
2024-04-30 21:10:54 -0700

    morgan module added

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import express from "express";
 import newsRouter from "./routes/news/news.js";
 import usersRouter from "./routes/users/users.js";
 import queriesRouter from "./routes/queries/queries.js";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/news", newsRouter);
 app.use("/users", usersRouter);
